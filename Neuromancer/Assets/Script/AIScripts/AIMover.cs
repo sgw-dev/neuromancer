7ca2066f6cb31911aa95f73d9c116d904fff05ef
Spencer <sburke17@emich.edu>
2021-03-10 10:03:14 -0500

    Started work on AI search

---
+++ Neuromancer/Assets/Script/AIScripts/AIMover.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIMover : MonoBehaviour
+{
+    public GameObject agent;
+    public PointerController pc;
+
+    private HexTile hexTile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            hexTile = pc.HexTile;
+            agent.transform.position = hexTile.Position;
+        }
+    }
+}
---
+++ Neuromancer/Assets/Script/AIScripts/Search.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Search
+{
+    public Search()
+    {
+
+    }
+    public ArrayList GreedySearch(HexTile start, HexTile end)
+    {
+        ArrayList closeSet = new ArrayList();
+
+        /*
+        closeSet = set([])
+    fringe = util.PriorityQueue()
+
+    initialState = problem.getStartState()
+    fringe.update((initialState, []), 0)
+
+    while not fringe.isEmpty():
+        state, actions = fringe.pop()
+        if problem.isGoalState(state):
+            return actions
+        if state not in closeSet:
+        closeSet.add(state)
+            successors = problem.getSuccessors(state)
+            #successors.append((nextState, actions, cost))
+            #successors.reverse()
+            for nextState, action, cost in successors:
+                new_actions = actions + [action]
+                h_val = heuristic(nextState, problem);
+                g_val = problem.getCostOfActions(new_actions)
+                fringe.update((nextState, new_actions), h_val + g_val)
+                */
+        return closeSet;
+    }
+    
+    public class PriorityQueue
+    {
+
+    }
+}
