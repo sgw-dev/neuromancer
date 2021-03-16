7c10f528b5d3d8d83e784a5b5fbcf98dc5082418
Spencer <sburke17@emich.edu>
2021-03-16 11:48:38 -0400

    Working on placing characters

--- Neuromancer/Assets/Script/AIScripts/AIMover.cs
+++ Neuromancer/Assets/Script/AIScripts/AIMover.cs
@@ -1,30 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TurnBasedSystem;
 using UnityEngine;
 
 public class AIMover : MonoBehaviour
 {
     public GameObject agent;
-    public PointerController pc;
+    public PointerController pointer;
     public HexTileController htc;
 
     private HexTile destTile;
     private bool movingFlag = false;
 
-    
+    private Player player;
+
+    CharacterClass[] classes =
+        new CharacterClass[] {
+                CharacterClass.MELEE,
+                CharacterClass.RANGED,
+                CharacterClass.PSYONIC,
+                CharacterClass.HACKER
+            };
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //player = GameSystem.CurrentGame().Players()[1];
     }
 
     // Update is called once per frame
     void Update()
     {
+        player = GameSystem.CurrentGame().Players()[1];
+        //If it is my turn
+        if (player.name.Equals(GameSystem.CurrentGame().WhosTurn()))
+        {
+            foreach(CharacterClass cc in classes)
+            {
+                Character character = player.characters[cc.ToString()];
+            }
+        }
+        /*
         if (Input.GetButtonDown("Fire1") && !movingFlag)
         {
-            destTile = pc.HexTile;
+            destTile = pointer.HexTile;
             
             HexTile startTile = htc.FindHex(agent.transform.position);
             if (!destTile.Equals(startTile))
@@ -35,7 +54,7 @@ public class AIMover : MonoBehaviour
             }
             
             
-        }
+        }*/
     }
     public IEnumerator Move(List<int> path, float frameTime)
     {
