c49296ae094ae2b3440ba556bdc0f83259c87ebc
Greg <goyafuso.voided@gmail.com>
2021-03-24 20:54:13 -0700

    Character Death

---
+++ Neuromancer/Assets/TurnBasedSystem/MarkedForDeath.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace TurnBasedSystem
+{
+    public class MarkedForDeath : MonoBehaviour
+    {
+        
+        public bool die=false;
+        float timetodie=float.MaxValue;
+        float time = 0;
+        void Update()
+        {
+            if(die) {
+                time+=Time.deltaTime;
+            }
+            if(time>timetodie) {
+                Death();
+            }
+        }
+
+
+        public void Setup(float time_to_die)
+        {
+            timetodie=time_to_die;
+            die=true;
+        }
+
+
+        public void Death()
+        {
+            //some animation stuff or something idk
+            Destroy(this.gameObject);
+        }
+    }
+}
